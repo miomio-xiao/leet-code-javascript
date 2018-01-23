eeb758579f926404d03e593ee8384b29f3250d60
miomio-xiao <miomio.xiao@gmail.com>
2018-01-23 14:09:08 +0800

    21. Merge Two Sorted Lists

---
+++ code/21. Merge Two Sorted Lists/mergeTwoLists.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+  let p = new ListNode(-1);
+  let h = p;
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      p.next = l1;
+      l1 = l1.next;
+    } else {
+      p.next = l2;
+      l2 = l2.next;
+    }
+    p = p.next;
+  }
+  p.next = l1 ? l1 : l2;
+  return h.next;
+};
+
+/**
+ * 递归的方式
+ *
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+  if (!l1) return l2;
+  if (!l2) return l1;
+
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
+  }
+};
